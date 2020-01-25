9a1657c02a4375776c8937629a40c172994528d4
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-25 07:30:14 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, age) VALUES ("kheshen", 24), ("avi", 30), ("karen", 22), ("shivanie", 16), ("renita", 21), ("shlesma", 25), ("tarric", 24), ("melissa", 22), ("haresh", 25), ("chris", 24), ("dave", 26), ("pooran", 26), ("derek", 24), ("jeslyn", 23), ("farhana", 24), ("dolma", 26), ("mingying", 27), ("chrystal", 28), ("john", 24), ("ryan", 23);
+
+INSERT INTO projects (title) VALUES ("compass"), ("ateaz"), ("red pipe"), ("leaisure"), ("oasis"), ("monster"), ("white noise"), ("comma"), ("java"), ("kunst");
+
+INSERT INTO pledges (amount) VALUES (1), (22), (2121), (444), (4), (67), (7), (77777), (78), (35), (23), (42), (567), (9), (6), (555), (666), (44), (9), (10), (154), (554), (2), (3), (37), (34), (55), (777), (767), (696);
